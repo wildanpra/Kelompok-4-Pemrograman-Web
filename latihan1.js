8de902406b96ee599d0136da2e4bd2d9e426cd5f
MOHAMMAD WILDAN PRATAMA <810202606@intranet.pajak.go.id>
2026-06-15 13:29:14 +0700

    first commit

---
+++ latihan1.js
@@ -0,0 +1,11 @@
+const nama = "Arip";
+const umur = 29;
+const isMarried = true;
+// const dateAt;
+
+console.log(typeof nama, typeof umur, typeof isMarried);
+
+console.log(nama, umur, isMarried);
+
+const greeting = `Hello, my name ${nama}.`;
+console.log(greeting);
