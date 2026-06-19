fce48369ad1a8f9f592a4504520901eff6fc7860
Wildan <wildanpangandaran@gmail.com>
2026-06-19 08:51:20 +0700

    penambahan errorHandler.js

--- backend/validation/customerValidation.js
+++ backend/validation/customerValidation.js
@@ -3,6 +3,17 @@ const validateId = (id) => {
     return null;
 }
 
+const validateStore = (name, email, phone, company, status = {}) => {
+    const errors = [];
+    if (!name || name.trim() ) errors.push("Nama Pelanggan wajib diisi");
+    if (name && name.length > 100 ) errors.push("Nama Pelanggan maksimal 100 karakter");
+    if (email && email.length >100 ) errors.push("Email maksimal 100 karakter");
+    if (phone && phone.length > 20 ) errors.push("Nomor Telepon maksimal 20 karakter");
+
+    return errors.length ? errors : null;
+}
+
+const validateUpdate = validateStore;
 module.exports = {
-    validateId
+    validateId, validateStore, validateUpdate
 };
