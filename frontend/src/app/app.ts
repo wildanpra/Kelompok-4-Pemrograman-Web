bf4ce5dc1158e9fd84c0f34cfd344557adfe9a68
MOHAMMAD WILDAN PRATAMA <810202606@intranet.pajak.go.id>
2026-06-23 09:13:06 +0700

    install angular

---
+++ frontend/src/app/app.ts
@@ -0,0 +1,15 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Sidebar } from './components/sidebar/sidebar';
+import { Footer } from './components/footer/footer';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Header, Sidebar, Footer],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {
+  protected readonly title = signal('frontend');
+}
