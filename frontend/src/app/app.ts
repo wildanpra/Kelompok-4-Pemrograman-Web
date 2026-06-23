d91f39ebaf9c8c435e2b4f3ec084dc81c0a434ff
MOHAMMAD WILDAN PRATAMA <810202606@intranet.pajak.go.id>
2026-06-23 13:31:22 +0700

    update

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,14 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './components/header/header';
-import { Sidebar } from './components/sidebar/sidebar';
-import { Footer } from './components/footer/footer';
-import { CustomerComponent } from './pages/customer/customer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Sidebar, Footer, CustomerComponent],
-  templateUrl: './app.html',
+  imports: [RouterOutlet],
+  // templateUrl: './app.html',
+  template: `<router-outlet></router-outlet>`,
   styleUrl: './app.css',
 })
 export class App {
