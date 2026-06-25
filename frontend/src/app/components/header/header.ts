38b46546a06fa565fe2ae99db9ef0fbb1614a18a
Vina Sari <vinamursyid@gmail.com>
2026-06-25 13:42:58 +0700

    penambahan logout

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { AuthService } from '../../service/auth_service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-header',
@@ -6,4 +9,34 @@ import { Component } from '@angular/core';
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private ngZone: NgZone,
+  ) {}
+  onLogout(): void {
+    Swal.fire({
+      title: 'Logout?',
+      text: 'Anda akan keluar dari aplikasi',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#d33',
+      cancelButtonColor: '#6c757d',
+      confirmButtonText: 'Ya, Keluar',
+      cancelButtonText: 'Batal',
+    }).then((result) => {
+      if (!result.isConfirmed) return;
+      this.auth.logout();
+      Swal.fire({
+        title: 'Berhasil!',
+        text: 'Berhasil Logout',
+        icon: 'success',
+        timer: 1500,
+        showConfirmButton: false,
+      }).then(() => {
+        this.ngZone.run(() => this.router.navigate(['/login']));
+      });
+    });
+  }
+}
